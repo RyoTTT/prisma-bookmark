19517a864f0cd7668fb5973f2f61021360995964
RTTT <rttt98520@gmail.com>
2023-06-28 15:06:15 +0900

    .hasky削除

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,36 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'standard-with-typescript',
-    'plugin:react/recommended',
-    'prettier'
-  ],
+  extends: ["standard-with-typescript", "plugin:react/recommended", "prettier"],
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: "script",
+      },
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ["react"],
   rules: {
-    'react/react-in-jsx-scope': 'off',
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/consistent-type-imports": "off",
   },
-  // 末尾に以下を追加する
-settings: {
-  react: {
-    version: '18.2.0'
-  }
-}  
-}
+  settings: {
+    react: {
+      version: "18.2.0",
+    },
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import Header from "../components/Header";
+import AuthWrapper from "../components/AuthWrapper";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <AuthWrapper>
+        <Header />
+        <Component {...pageProps} />
+      </AuthWrapper>
+    </SessionProvider>
+  );
 }
+
+export default MyApp;
